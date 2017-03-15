e83ec7e02fa64d03fde814ad1ca770305561d971
王晗 <hanwang.0501@gmail.com>
2017-03-15 10:25:52 +0800

    Initial commit

---
+++ src/main/java/cn/sc/scu/constants/Constants.java
@@ -0,0 +1,22 @@
+package cn.sc.scu.constants;
+
+/**
+ * 配置常量接口类
+ * <p>
+ * Created by Wanghan on 2017/3/15.
+ * Copyright © Wanghan SCU. All Rights Reserved
+ */
+
+public interface Constants {
+    String SPARK_MASTER = "local";
+    String SPARK_LOCAL = "spark.local";
+
+    String JDBC_URL = "jdbc.url";
+    String JDBC_USER = "jdbc.user";
+    String JDBC_DRIVER = "jdbc.driver";
+    String JDBC_PASSWORD = "jdbc.password";
+    String JDBC_USER_PROD = "";
+    String JDBC_URL_PROD = "";
+    String JDBC_PASSWORD_PROD = "";
+    String JDBC_DATASOURCE_SIZE = "jdbc.datasource.size";
+}
