00daadbe996f2b8b2d0f2f5d60ac91129a6dae91
王晗 <hanwang.0501@gmail.com>
2017-03-15 16:45:58 +0800

    Update SQL Model

---
+++ src/main/java/cn/sc/scu/javautils/StringUtils.java
@@ -0,0 +1,9 @@
+package cn.sc.scu.javautils;
+
+/**
+ * Created by Wanghan on 2017/3/15.
+ * Copyright © Wanghan SCU. All Rights Reserved
+ */
+public class StringUtils {
+
+}
--- src/main/scala/cn/sc/scu/analyse/realTimeAnalyse.scala
+++ src/main/scala/cn/sc/scu/analyse/realTimeAnalyse.scala
@@ -9,51 +9,54 @@ import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
   */
 object realTimeAnalyse {
 
-  /**
-    * 计算用户点击广告次数
-    * 返回格式 <"time_userid_adid",times>
-    * @param streamingContext
-    * @param data
-    * @return
-    */
-  def countClickTimes(streamingContext:StreamingContext, data:ReceiverInputDStream[(String, String)]): DStream[(String, Int)] ={
+    /**
+      * 计算用户点击广告次数
+      * 返回格式 <"time_userid_adid",times>
+      *
+      * @param streamingContext
+      * @param data
+      * @return
+      */
+    def countClickTimes(streamingContext: StreamingContext, data: ReceiverInputDStream[(String, String)]): DStream[(String, Int)] = {
 
-    val logs = data.map(_._2).flatMap(_.split("\n")).map(row => {
-      val array = row.split("\t").toBuffer
-      array
-    })
+        val logs = data.map(_._2).flatMap(_.split("\n")).map(row => {
+            val array = row.split("\t").toBuffer
+            array
+        })
 
-    val advertisements = logs.map(r =>{
-      (s"${r.head}_${r(3)}_${r(4)}",1)
-    })
+        val advertisements = logs.map(r => {
+            (s"${r.head}_${r(3)}_${r(4)}", 1)
+        })
 
-    def updateFunc(iterator: Iterator[(String, Seq[Int], Option[Int])]): Iterator[(String, Int)] = {
-      iterator.flatMap { case (x, y, z) => Some(y.sum + z.getOrElse(0)).map(i => (x, i)) }
+        def updateFunc(iterator: Iterator[(String, Seq[Int], Option[Int])]): Iterator[(String, Int)] = {
+            iterator.flatMap { case (x, y, z) => Some(y.sum + z.getOrElse(0)).map(i => (x, i)) }
+        }
+
+        val adCounts = advertisements.updateStateByKey(updateFunc _,
+            new HashPartitioner(streamingContext.sparkContext.defaultParallelism), rememberPartitioner = true)
+
+        adCounts
     }
 
-    val adCounts = advertisements.updateStateByKey(updateFunc _,
-      new HashPartitioner(streamingContext.sparkContext.defaultParallelism), rememberPartitioner = true)
+    /**
+      * 获取黑名单：用户每天点击某个广告次数超过100次
+      *
+      * @param ds
+      * @return
+      */
+    def getBlackList(ds: DStream[(String, Int)]): DStream[(String, Int)] = {
+        ds.filter(_._2 > 100)
+    }
 
-    adCounts
-  }
-
-  /**
-    * 获取黑名单：用户每天点击某个广告次数超过100次
-    * @param ds
-    * @return
-    */
-  def getBlackList(ds:DStream[(String, Int)]): DStream[(String, Int)] ={
-    ds.filter(_._2>100)
-  }
-
-  /**
-    * 过滤黑名单用户
-    * @param ds
-    * @param blackList
-    * @return
-    */
-  def clickTimesFilter(ds:DStream[(String, Int)],blackList:List[String]): DStream[(String, Int)] ={
-    ds.filter(row => !blackList.contains(row._1))
-  }
+    /**
+      * 过滤黑名单用户
+      *
+      * @param ds
+      * @param blackList
+      * @return
+      */
+    def clickTimesFilter(ds: DStream[(String, Int)], blackList: List[String]): DStream[(String, Int)] = {
+        ds.filter(row => !blackList.contains(row._1))
+    }
 
 }
